758c6036f182bd0c4189ffacdf94812dd8a60473
Roxy Farkas <rsfarkas@users.noreply.github.com>
2017-03-06 17:07:31 -0500

    updated sheep sleep animated

--- AnimatedSheepSleep/SpaceShooter/GameScene.swift
+++ AnimatedSheepSleep/SpaceShooter/GameScene.swift
@@ -1,23 +1,22 @@
+
 //
 //  GameScene.swift
 //  SheepCounter
 //
 //  Created by Roxanne Farkas on 2/11/17.
-
 import SpriteKit
 import GameplayKit
 
 //set global variable
 
-
 class GameScene: SKScene, SKPhysicsContactDelegate {
     // "global to object"
-
     var gameScore: SKLabelNode?
     var defeatedLabel: SKLabelNode?
     
     var currentScore = 0
     var currentFrame = 0
+    var countDown = 5.0
     
     //properties, define must "?"
     var player: SKSpriteNode?  //optional, "nil"
@@ -27,9 +26,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let enemyCategory:UInt32 = 0b1 << 2
     let itemCategory:UInt32 = 0b1 << 3
     //var highScore: SKLabelNode?
-
     var zShots: SKSpriteNode?
-  
+    
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
         
@@ -60,13 +58,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.addChild(zShots!)
         
+        
+        zShots?.run(
+            SKAction.sequence([
+                SKAction.wait(forDuration: 4.0),
+                SKAction.removeFromParent()
+                ])
+            
+        )
+        
+        
     }
+    
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for  t in touches {
-            //print("t= \(t.location(in: self))")
             
             player?.position.x = t.location(in: self).x
             
@@ -74,20 +82,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
-
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
         for  t in touches {
-
+            
             // move player's X position to that point.
             player?.position.x = t.location(in: self).x
             
             // Shot!
             
-            print(currentFrame)
-            
-            if currentFrame % 5 == 0 {
-                print("Z shot")
+            if(currentFrame % 5 == 0){
                 makeZees()
             }
             
@@ -101,7 +106,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             // move player's X position to that point.
             player?.position.x = t.location(in: self).x
-
+            
             
         }
         
@@ -109,25 +114,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     func updateLabel(){
-        print("score update")
         gameScore = self.childNode(withName: "gameScore") as? SKLabelNode
         gameScore?.text = "Score: " + "\(currentScore)"
-
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         currentFrame += 1
         if (currentScore >= 100){
-            print("game won")
             endGame()
         }
         
-        
     }
-
+    
     func endGame(){
-       SoundManager.instance.play(soundFile: "background")
+        SoundManager.instance.play(soundFile: "background")
         UserDefaults.standard.set(currentScore, forKey: "lastScore")
         let pastTopScore = UserDefaults.standard.value(forKey: "topScore") as? Int ?? 0
         
@@ -136,7 +138,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         let nextScene = SKScene(fileNamed: "gameOver")
-        print("endingGame")
         nextScene?.scaleMode = .aspectFit
         self.view?.presentScene(nextScene)
     }
@@ -151,7 +152,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         let nextScene = SKScene(fileNamed: "loser")
-        print("losingGame")
         nextScene?.scaleMode = .aspectFit
         self.view?.presentScene(nextScene)
     }
@@ -160,24 +160,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     func didBegin(_ contact: SKPhysicsContact) {
-
-    
+        
+        
         if ((contact.bodyA.contactTestBitMask == 2)  && (contact.bodyB.contactTestBitMask == 1)){
             whenZhitsSheep(zshot: contact.bodyA.node)
         }
         
         if ((contact.bodyB.contactTestBitMask == 2)  && (contact.bodyA.contactTestBitMask == 1)){
-           whenZhitsSheep(zshot: contact.bodyB.node)
+            whenZhitsSheep(zshot: contact.bodyB.node)
         }
         
         if(contact.bodyA.contactTestBitMask == 1) && (contact.bodyB.contactTestBitMask == 7){
-                print("callingloser")
-                updateLabel()
-                loser()
-     }
-
+            updateLabel()
+            loser()
+        }
+        
         if(contact.bodyA.contactTestBitMask == 7) && (contact.bodyB.contactTestBitMask == 1){
-            print("callingloser")
             
             updateLabel()
             loser()
@@ -185,32 +183,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     
-    // when z hits sheep
-    // removes z from the parent
-    // updates the score
-    // updates the score label
-    
-   // func zTimeOut(zshot: SKNode?){
-     // if currentFrame % 2 == 0 {
-       // zShots?.removeFromParent()
-   // }
-        
-   //}
     func whenZhitsSheep(zshot: SKNode?) {
+        
         SoundManager.instance.play(soundFile: "bah1")
         zshot?.removeFromParent()
         currentScore = currentScore + 1
         updateLabel()
     }
     
-    //if frame rate has incremented 4 times since the z was shot
-    // zshot?.removeFromParent()
-    //call this function in update label
     
-    //func zTimeOut(currentFrame, zshot: SKNode?) {
-        
-      //  if(currentFrame)
-        // zshot?.removeFromParent()
-   // }
-
+    
 }
