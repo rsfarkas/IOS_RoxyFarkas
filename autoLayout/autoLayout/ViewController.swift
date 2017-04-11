b8f6de4c690c3547843d145f776f26cd04f67b13
Roxanne Farkas <roxannesfarkas@gmail.com>
2017-04-11 15:45:20 -0400

    calc modifications

---
+++ autoLayout/autoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Roxanne Farkas on 4/6/17.
+//  Copyright © 2017 Roxanne Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
